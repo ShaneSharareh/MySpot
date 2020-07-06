7767d891954bd4ec9e516763d8db51700ff626e5
Shane Sharareh <shane.s.sharareh@gmail.com>
2020-07-06 09:17:54 -0700

    Initial commit with the main activity and add a spot activity

---
+++ app/src/main/java/com/example/spotty/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.spotty;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+FloatingActionButton btnAddPlace;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnAddPlace = (FloatingActionButton) findViewById(R.id.addfavoriteplace);
+        btnAddPlace.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startAddPage();
+            }
+        });
+    }
+
+    private void startAddPage(){
+       Intent intent = new Intent(this, AddSpot.class);
+        startActivity(intent);
+    }
+}
